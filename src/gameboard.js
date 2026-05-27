ffde9bb2d07fbc9ed7cbb543cdeaced92bfd1aeb
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-27 08:14:57 +0200

    Fix gameboard logic for duplicate attacks

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,6 +2,7 @@ class Gameboard {
   constructor() {
     this.ships = [];
     this.missedAttacks = [];
+    this.attacks = [];
   }
 
   placeShip(ship, x, y, direction = "horizontal") {
@@ -19,22 +20,28 @@ class Gameboard {
   }
 
   receiveAttack(x, y) {
+    const duplicateAttack = this.attacks.find(
+      (attack) => attack.x === x && attack.y === y,
+    );
+
+    if (duplicateAttack) return false;
+
+    this.attacks.push({ x, y });
+
     const attackedShip = this.ships.find((shipData) =>
       shipData.coordinates.some(
         (coordinate) => coordinate.x === x && coordinate.y === y,
       ),
     );
+
     if (attackedShip) {
       attackedShip.ship.hit();
-      return;
+      return true;
     }
 
-    const duplicateAttack = this.missedAttacks.find(
-      (attack) => attack.x === x && attack.y === y,
-    );
-    if (duplicateAttack) return;
-
     this.missedAttacks.push({ x, y });
+
+    return true;
   }
 
   allShipsSunk() {
