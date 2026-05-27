186203cf085383ca8674fca42363581ead560348
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-27 21:44:26 +0200

    Fix ship placement validation and add placeRandomShip

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,16 +7,47 @@ class Gameboard {
 
   placeShip(ship, x, y, direction = "horizontal") {
     let coordinates = [];
+    if (x + ship.length > 10 && direction === "horizontal") return false;
+    if (y + ship.length > 10 && direction === "vertical") return false;
+
     if (direction === "horizontal") {
-      for (let i = 0; i < ship.length; i++) {
-        coordinates.push({ x, y: y + i });
-      }
-    } else {
       for (let i = 0; i < ship.length; i++) {
         coordinates.push({ x: x + i, y });
       }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        coordinates.push({ x, y: y + i });
+      }
     }
+
+    if (
+      this.ships.some((shipData) =>
+        shipData.coordinates.some((coordinate) =>
+          coordinates.some(
+            (cord) => coordinate.x === cord.x && coordinate.y === cord.y,
+          ),
+        ),
+      )
+    ) {
+      return false;
+    }
+
     this.ships.push({ ship, coordinates });
+    return true;
+  }
+
+  placeRandomShip(ship) {
+    let placed = false;
+    while (!placed) {
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
+      let direction = "horizontal";
+      const random_boolean = Math.random() < 0.5;
+      if (random_boolean) {
+        direction = "vertical";
+      }
+      placed = this.placeShip(ship, x, y, direction);
+    }
   }
 
   receiveAttack(x, y) {
